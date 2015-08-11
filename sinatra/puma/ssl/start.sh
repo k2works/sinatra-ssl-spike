6d6068bc2d5a9c3a7429ded2f25d27efe7d9779f
k2works <kakimomokuri@gmail.com>
2015-08-11 19:05:40 +0900

    APサーバーmonit対応

--- sinatra/puma/ssl/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-service nginx start
-bundle exec puma --config config/puma.rb -d
-
-while true
-do
-    sleep 10
-done
--- sinatra/unicorn/ssl/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-service nginx start
-bundle exec unicorn -c unicorn.conf -D
-
-while true
-do
-    sleep 10
-done
